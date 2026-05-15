d5ec5a52be4d9e3bf9818edb151ea5ed850c0a7e
runeeie-png <runeeie@outlook.com>
2026-05-15 17:01:52 +0200

    Update tts.js

--- api/tts.js
+++ api/tts.js
@@ -24,7 +24,8 @@ export default async function handler(req, res) {
         },
       body: JSON.stringify({
   text: text,
-  model_id: "eleven_multilingual_v2",
+  model_id: "eleven_multilingual_v2_5",
+        language_code: "no",
   voice_settings: {
             stability: 0.6,
             similarity_boost: 0.75,
