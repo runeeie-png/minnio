20b92da38007dd0ea9e1567c4abad6e2a3f988e9
runeeie-png <runeeie@outlook.com>
2026-05-16 13:59:10 +0200

    Delete sessions-overview.js

--- sessions-overview.js
+++
@@ -1,7 +0,0 @@
-{
-  "rewrites": [
-    { "source": "/", "destination": "/admin.html" },
-    { "source": "/s/:id", "destination": "/s/index.html" },
-    { "source": "/api/session/:id", "destination": "/api/session/[id]" }
-  ]
-}
